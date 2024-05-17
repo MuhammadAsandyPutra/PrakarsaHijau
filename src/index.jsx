38876bb53dd3107d35dae8b09de1448d8b44b979
Muhammad Asandy Putra <asandyputra77@gmail.com>
2024-05-17 17:34:53 +0800

    template commit

---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+// import { store } from './redux/index.js';
+// import { Provider } from 'react-redux';
+// import LoadingBar  from 'react-redux-loading-bar';
+import { StrictMode } from 'react';
+import { Routers } from './routes/Routes.jsx'
+
+const theme = extendTheme ({
+    styles: {
+        global: {
+            body: {
+                bg: 'gray.50',
+            },
+        },
+    },
+})
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+    {/* <Provider store={store}> */}
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+          {/* <LoadingBar /> */}
+
+          <Routers />
+        </ChakraProvider>
+      </BrowserRouter>
+    {/* </Provider> */}
+  </StrictMode>,
+)
